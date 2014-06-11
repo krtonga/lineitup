1e473d711909c4c6031cd2f013bb2ae78d5b9fe9
Kristen Tonga <kristen.tonga@gmail.com>
2014-06-11 16:16:00 -0400

    added show, edit, update, and destroy actions in controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,24 +2,36 @@
 class EventsController < ApplicationController
 
   def index
+    @events = Event.all
   end
 
   def show
+    @event = Event.new(event_params)
   end
 
   def new
   end
 
   def create
+
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
 
   def update
+    @event = Event.find(params[:id])
+    @event.update(event_params)
   end
 
   def destroy
+    event.delete(params[:id])
   end
+
+private
+
+event_params = params.require(:event).permit(:name, :category)
+
 
 end
