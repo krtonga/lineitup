31559397fc9c869fd08673ae00f9f18248f8f817
jsmythd1 <jeffdrakos@gmail.com>
2014-06-11 16:14:58 -0400

    changes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,21 +5,29 @@ class EventsController < ApplicationController
   end
 
   def show
+    @name = name.find(params[:id])
   end
 
   def new
+
   end
 
   def create
   end
 
   def edit
+    @name = name.find(params[:id])
   end
 
   def update
+    edited_name = Name.find(params[:id])
+    name.update(params.require(:name).permit(:name)
+    redirect_to "/name/#{name.id}"
   end
 
   def destroy
+    Name.find(params[:id])
+    redirect_to '/names'
   end
 
 end
