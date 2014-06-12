214c576cd4f5a0557afaa3a0a7372fde8b28a117
jwink <jeff_winkler@yahoo.com>
2014-06-12 17:08:17 -0400

    with basic user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
 
-  root 'events#index'
-  resources :events
+  get '/login' => 'sessions#new', as: 'login'
+  post '/sessions' => 'sessions#create', as: 'sessions'
+  delete '/logout' => 'sessions#destroy', as: 'logout'
+
+  get '/signup' => 'users#new', as: 'signup'
+  post '/users' => 'users#create', as: 'users'
+
+  get '/profile' => 'users#profile', as: 'profile'
+
+
+  #resources :events
+
+
+
+  root 'welcome#index'
 
 end
