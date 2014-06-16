3a0de92088d93dfec0199f83398299abab0692e5
jwink <jeff_winkler@yahoo.com>
2014-06-16 15:50:59 -0400

    better date stuff

--- app/assets/javascripts/matchdates.js
+++ app/assets/javascripts/matchdates.js
@@ -2,10 +2,18 @@
 
 
 function matchDates(event) {
+  var weekday = new Array(7);
+  weekday[0]=  "sun";
+  weekday[1] = "mon";
+  weekday[2] = "tue";
+  weekday[3] = "wed";
+  weekday[4] = "thu";
+  weekday[5] = "fri";
+  weekday[6] = "sat";
   var start = $('#start_date_for_range').text();
   var end = $('#end_date_for_range').text();
-  var range_start = new Date(Date.parse(start));
-  var range_end = new Date(Date.parse(end));
+  range_start = new Date(Date.parse(start));
+  range_end = new Date(Date.parse(end));
   var dateArray = [];
   if (event.eventDateList != undefined) {
     $.each(event.eventDateList, function(index, date) {
@@ -17,6 +25,11 @@ function matchDates(event) {
       }
     });
   } else {
+
+    for (var d = range_start; d <= range_end; d.setDate(d.getDate() + 1)) {
+      console.log(weekday[d.getDay()], d);
+      //  need to get start recurring and end recurring as dates, check
+    }
 
   }
   if (dateArray.length > 0) {
