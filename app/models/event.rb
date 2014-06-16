cac78be1bd81b2ce28a70545491b2620718439f1
jwink <jeff_winkler@yahoo.com>
2014-06-16 12:56:24 -0400

    filter string update

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ActiveRecord::Base
   has_many :categories, through: :haps
 
   def self.pullAPI(category_filter)
-    results = HTTParty.get('http://api.nytimes.com/svc/events/v2/listings.json?&filters=category:'+category_filter+'&api-key='+KEY1+'&limit=5000')
+    results = HTTParty.get('http://api.nytimes.com/svc/events/v2/listings.json?'+category_filter+'&api-key='+KEY1+'&limit=5000')
     get_uniq_results = {}
     results["results"].each do |event|
       get_uniq_results[event["event_id"]] = event
@@ -29,7 +29,11 @@ class Event < ActiveRecord::Base
         search_string += category + "+"
       end
     end
-    return "(" + search_string.chop + ")"
+    return "&filters=category:(" + search_string.chop + ")"
+  end
+
+  def self.make_search_query(params)
+
   end
 
 end
